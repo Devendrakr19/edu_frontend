bf12d3b4adf1bb332b70d6767c88a67d96751b70
Devendrakr19 <dk0509109@gmail.com>
2024-09-22 23:39:13 +0530

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -39,8 +39,7 @@ const Footer = () => {
             <button
               type="submit"
               className="text-white absolute end-2.5 bottom-[9px] 
-                
-                          bg-[#0BA085] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-6 py-3 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+               bg-[#0BA085] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-full text-sm px-6 py-3 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Join now
             </button>
